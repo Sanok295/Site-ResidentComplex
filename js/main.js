6cb610a4ac8e4c6a20043f376cd0c6808b1f5b54
sanok295 <sanok295@mail.ru>
2025-06-03 22:02:54 +0300

    finished site except image bg-image in github

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-// let apartmentItems = document.querySelectorAll('.apartments__grid-item');
+const apartments = document.querySelectorAll('.apartments__grid-item');
 
+apartments.forEach(item =>{
+    item.addEventListener('click', function(){
+        const bg = this.querySelector('.apartments__grid-item__bg');
 
+        if (bg) {
+            item.classList.toggle('scaleAparts');
+            bg.classList.toggle('vieAparts');
+        }
+    })
+})
